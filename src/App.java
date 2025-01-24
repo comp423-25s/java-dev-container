a815d294701e205b0c11e85985f5c8e8ad4f740f
Kris Jordan <kris@cs.unc.edu>
2025-01-24 13:39:02 +0000

    Update starter example

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
-public class App {
+import java.util.List;
+
+class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        var input = List.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        var output = input.stream().map((x) -> Math.pow(2, x)).toList();
+        System.out.println(input);
+        System.out.println(output);
     }
 }
